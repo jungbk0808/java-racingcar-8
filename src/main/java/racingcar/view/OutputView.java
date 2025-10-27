3781502d41557519a6cc6157e7e2a62117553ace
jungbk0808 <jungbk0808@gmail.com>
2025-10-27 17:23:30 +0900

    refactor(OutputView): 출력 메시지 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,16 +7,18 @@ import java.util.List;
 public class OutputView {
     private static final String DISTANCE_SYMBOL = "-";
     private static final String CAR_NAME_SEPARATOR = ", ";
+    private static final String RESULT_SEPARATOR = " : ";
+    private static final String WINNER_OUTPUT_PREFIX = "최종 우승자" + RESULT_SEPARATOR;
 
     public void printRacingCarDistance(RacingCar racingCar) {
-        String output = racingCar.getCarName() + " : " +
+        String output = racingCar.getCarName() + RESULT_SEPARATOR +
                 DISTANCE_SYMBOL.repeat(racingCar.getDistance());
 
         this.printOutput(output);
     }
 
     public void printRacingWinner(List<RacingCar> winners) {
-        StringBuilder output = new StringBuilder("최종 우승자 : ");
+        StringBuilder output = new StringBuilder(WINNER_OUTPUT_PREFIX);
 
         winners.forEach(racingCar -> {
             output.append(racingCar.getCarName());
